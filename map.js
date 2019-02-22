77ce19f065801b0bfc23b41abdb3872625d7ee3a
yqjim111 <yangqi2017@outlook.com>
2019-02-22 12:35:24 -0500

    2

--- map.js
+++ map.js
@@ -6,6 +6,10 @@ console.log('Loaded map.js')
 // your mapbox token
 mapboxgl.accessToken = 'pk.eyJ1IjoieXFqaW0xMTEiLCJhIjoiY2psb2k5ZGZkMXR1czNxdDV3dW55b3hyaiJ9.ihJM3eJ8hNrDmr7EYKjo7w'
 
+'use strict'
+
+console.log('Loaded map.js')
+
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/brianhouse/cjn0u552b52kr2spdz6yhpqj4',
@@ -21,6 +25,8 @@ let navigation = new mapboxgl.NavigationControl({
 
 // add the navigation to your map
 map.addControl(navigation, 'top-left')
+
+
 
 // create an instance of ScaleControl
 let scale = new mapboxgl.ScaleControl({
@@ -43,6 +49,11 @@ let geolocate = new mapboxgl.GeolocateControl({
 
 map.addControl(geolocate, 'top-left')
 
+// this is an event handler
+geolocate.on('geolocate', function(event) {
+    console.log(event.coords)
+})
+
 geolocate.on('geolocate', function(event) {
 
     // create new variables to store the attributes we're interested in from the event
@@ -53,6 +64,17 @@ geolocate.on('geolocate', function(event) {
     console.log('geolocated:', lng, lat)
 
     // format lng lat values and display them on our 'info' element
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+map.on('click', function(event) {
+
+    let lng = event.lngLat.lng
+    let lat = event.lngLat.lat
+
+    console.log("clicked:", lng, lat)
+
     document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
 
 })
