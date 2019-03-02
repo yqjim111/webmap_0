6360ac177ed925a5d13c683b712385562da4addf
yqjim111 <yangqi2017@outlook.com>
2019-03-01 22:31:30 -0500

    Qi

--- map.js
+++ map.js
@@ -12,15 +12,15 @@ console.log('Loaded map.js')
 
 let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/brianhouse/cjn0u552b52kr2spdz6yhpqj4',
-    center: [-73.96216,40.80779],
+    style: 'mapbox://styles/mapbox/basic-v9',
+    center: [107.51264,26.26247],
     zoom: 16,
     pitch: 45
 })
 
 // create an instance of NavigationControl
 let navigation = new mapboxgl.NavigationControl({
-    showCompass: false
+    showCompass: true
 })
 
 // add the navigation to your map
@@ -76,5 +76,48 @@ map.on('click', function(event) {
     console.log("clicked:", lng, lat)
 
     document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+let marker = new mapboxgl.Marker()
+marker.setLngLat([-73.96007,40.80871])
+marker.addTo(map)
+
+let popup = new mapboxgl.Popup()
+popup.setHTML('This is the Center for Spatial Research<br /><img src="https://currystonefoundation.org/wp-content/uploads/2018/05/csf_pr_csr_image5.jpg" />')
+marker.setPopup(popup)
+
+let data = [
+    {
+        location: [107.51309,26.25557],
+        content: 'I attended a lot of cram schools here'
+    },
+    {
+        location: [107.51554,26.25428],
+        content: 'The park that most citizens went during weekends when I was young'
+    },
+    {
+        location: [107.51164,26.26535],
+        content: 'City center of my hometown'
+    },
+    {
+        location: [107.51769,26.25589],
+        content: 'I learned how to play table tennis'
+    },
+    {
+        location: [107.51426,26.25150],
+        content: 'The island has been renovated into commercial center'
+    }
+    ]
+
+data.forEach(function(d) {
+
+    let marker = new mapboxgl.Marker()    
+    marker.setLngLat(d.location)
+    marker.addTo(map)  
+
+    let popup = new mapboxgl.Popup()
+    popup.setHTML(d.content)
+    marker.setPopup(popup)
 
 })
